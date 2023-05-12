5b5f2a3ba50af8d960e05f2d8e2e4e8377a0dbba
alicestoneprod <noyname.name2017@ya.ru>
2023-05-12 16:59:53 +0300

    иди нахуй

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const axios = require('axios');
 
 
 
+const express = require('express');
+const app = express();
+
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
 
 
 let time = new Date();
