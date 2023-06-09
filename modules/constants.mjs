b81f62ff6cef83698f962c504c65436f74deb931
alicestoneprod <noyname.name2017@ya.ru>
2023-06-10 00:38:04 +0300

    1

--- modules/constants.mjs
+++
@@ -1,6 +0,0 @@
-const WeatherKey = "550f3c04fdcb23cdcb3ad42f6392fd92"
-const TelegramToken = "5815916630:AAFL-k70clz6u3AV_vK8jh_FmnFQ7VjtH9o"
-const themes = "18+, про мента, 4, для друзей"
-const time = new Date()
-
-export { WeatherKey, TelegramToken, themes, time }
