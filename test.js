52ba224cce1af91192edbb93730779e759bc5977
alicestoneprod <noyname.name2017@ya.ru>
2023-06-10 00:39:32 +0300

    1
    мяу

--- test.js
+++
@@ -1,9 +0,0 @@
-fetch("https://blockchain.info/ru/ticker")
-  .then((response) => response.json())
-  .then((data) => {
-    const price = data.RUB.sell
-    console.log(price)
-  })
-  .catch((error) => {
-    console.error(error)
-  })
